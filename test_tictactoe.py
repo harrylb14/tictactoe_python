421b958541b5d0f84bf3cbd4ab3862a4a09655d2
Harry LB <harry.lingardbright@gmail.com>
2020-09-10 16:00:06 +0100

    working tic tac toe functionality

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -1,7 +1,87 @@
-from tictactoe import *
+from tictactoe import Game
 class Tests:
   def test_empty_board(self):
-    board = Board()
+    game = Game()
+    board = game.board
     assert board.state == [["-","-","-"],
                            ["-","-","-"],
                            ["-","-","-"]]
+
+  def test_playerX_go(self):
+    game = Game()
+    board = game.board
+    player = game.player1
+    player.move(board,0,0)
+    assert board.state == [["X","-","-"],
+                           ["-","-","-"],
+                           ["-","-","-"]]
+    assert game.turn == "PlayerO"
+  
+  def test_playerY_go(self):
+    game = Game()
+    board = game.board
+    player1 = game.player1
+    player2 = game.player2
+
+    player1.move(board,0,0)
+    player2.move(board,0,1)
+    assert board.state == [["X","O","-"],
+                           ["-","-","-"],
+                           ["-","-","-"]]
+    assert game.turn == "PlayerX"
+
+  def test_only_place_on_vacant_square(self, capsys):
+    game = Game()
+    board = game.board
+    player1 = game.player1
+    player2 = game.player2
+
+    player1.move(board,0,0)
+    player2.move(board,0,0)
+    out, err = capsys.readouterr()
+    assert "Square not vacant\n" in out
+    assert game.turn == "PlayerO"
+
+  def test_can_only_go_on_your_move(self, capsys):
+    game = Game()
+    board = game.board
+    player2 = game.player2
+
+    player2.move(board,0,0)
+    out, err = capsys.readouterr()
+    assert "Square not vacant\n" in out
+    
+  def test_winner_row(self):
+    game = Game()
+    game.board = [["X","X","X"],
+                  ["O","X","O"],
+                  ["-","-","-"]]
+    assert(game.winner() == "X")
+
+  def test_winner_column(self):
+    game = Game()
+    game.board = [["X","X","O"],
+                  ["O","X","O"],
+                  ["-","-","O"]]
+    assert(game.winner() == "O")
+
+  def test_winner_diagonal(self):
+    game = Game()
+    game.board = [["X","X","O"],
+                  ["O","X","O"],
+                  ["-","-","X"]]
+    assert(game.winner() == "X")
+
+  def test_game_in_progress(self):
+    game = Game()
+    game.board = [["X","X","O"],
+                  ["O","X","O"],
+                  ["-","-","-"]]
+    assert(game.winner() == "Game in progress")
+
+  def test_over_no_winner(self):
+    game = Game()
+    game.board = [["O","X","O"],
+                  ["O","X","O"],
+                  ["X","O","X"]]
+    assert(game.winner() == "No winner")
--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,59 @@
+
+class Game:
+  def __init__(self):
+    self.board = Board()
+    self.player1 = PlayerX(self, self.board)
+    self.player2 = PlayerO(self, self.board)
+    self.turn = "PlayerX"
+    self.game_over = False
+
+  def winning_player(self):
+    if sum(x.count('X') for x in self.board) > sum(x.count('O') for x in self.board):
+      return("X")
+    else:
+      return("O")
+
+  def winner(self):
+    if self.board[0][0] == self.board[0][1] == self.board[0][2] != '-' or self.board[1][0] == self.board[1][1] != '-' == self.board[1][2] or self.board[2][0] == self.board[2][1] == self.board[2][2] != '-':
+      return(self.winning_player())
+    elif self.board[0][0] == self.board[1][0] == self.board[2][0] != '-' or self.board[0][1] == self.board[1][1] == self.board[2][1] != '-' or self.board[0][2] == self.board[1][2] == self.board[2][2] != '-':
+      return(self.winning_player())
+    elif self.board[0][0] == self.board[1][1] == self.board[2][2] != '-' or self.board[0][2] == self.board[1][1] == self.board[2][0] != '-':
+      return(self.winning_player())
+    elif sum(x.count('-') for x in self.board) == 0:
+      return("No winner")
+    else:
+      return("Game in progress")
+  
+  
 class Board:
   def __init__(self):
     self.state = [["-","-","-"],
                   ["-","-","-"],
                   ["-","-","-"]]
+
+class PlayerX:
+  def __init__(self, game, board):
+    self.game = game 
+    self.board = board
+
+  def move(self, board, column, row):
+    if board.state[column][row] == "-" and self.game.turn == "PlayerX":
+      board.state[column][row] = "X" 
+      print(board.state)
+      self.game.turn = "PlayerO"
+    else:
+      print("Square not vacant")
+
+class PlayerO:
+  def __init__(self, game, board):
+    self.game = game 
+    self.board = board
+
+  def move(self, board, column, row):
+    if board.state[column][row] == "-" and self.game.turn == "PlayerO":
+      board.state[column][row] = "O" 
+      print(board.state)
+      self.game.turn = "PlayerX"
+    else:
+      print("Square not vacant")
